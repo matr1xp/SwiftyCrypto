432d30ea8e68296f7283b29a3c740a08b79d9158
Marlon Santos <marlonsantos.au@gmail.com>
2021-10-24 10:42:55 +1100

    Add Package.swift
    So we can import Swift package dependency in Xcode

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyCrypto",
+    platforms: [.iOS(.v10), .macOS(.v10_15)],
+    products: [
+        .library(name: "SwiftyCrypto", targets: ["SwiftyCrypto"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftyCrypto", path: "SwiftyCrypto")
+    ],
+    swiftLanguageVersions: [.v5])
